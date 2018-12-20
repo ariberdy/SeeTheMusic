b8458e26050997258edabf64701410194becfe9b
ariberdy <45810068+ariberdy@users.noreply.github.com>
2018-12-19 20:33:22 -0800

    Add download_bowie_wav.py to dev.
    This script creates a function that downloads a WAV file.

---
+++ download_bowie_wav.py
@@ -0,0 +1,16 @@
+""" Download Major Tom
+    Sample Block Comment
+
+    Multi-block block comment."""
+
+import urllib.request
+
+def download_bowie_wav():
+    """All Functions have comments like this"""
+    urllib.request.urlretrieve("http://www.wavsource.com/snds_2018-06-03_5106726768923853/music/space_oddity.wav", "bowie.wav")
+
+""" The function must be called for this to work.
+    Go to Python's command prompt.
+    Import this module (>>> import downlaod_bowie_wav)
+    Run the function using dot notation (>>> download_bowie_wav.download_bowie_wav())
+"""
